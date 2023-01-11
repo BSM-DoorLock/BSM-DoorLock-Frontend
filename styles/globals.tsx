a2a1ce403c3657d55db421d70bf07787a1b50acf
jeongdonggyeom <topcome928@gmail.com>
2023-01-11 17:53:07 +0900

    ADD :: 메인 페이지

--- styles/globals.tsx
+++ styles/globals.tsx
@@ -11,6 +11,8 @@ const GlobalStyle = createGlobalStyle`
     font-family: NotoSans, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     font-weight: bold;
+    margin: 0;
+    padding: 0;
   }
 
   @font-face {
