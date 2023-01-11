a179f509a146f74b35510619ba68e4e51d45eae9
jsm8109jsm <jsm8109jsm@gmail.com>
2023-01-11 15:23:52 +0900

    FEAT :: 방 공유 Alert

---
+++ components/alert/AlertBar.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+import { Alert, Button, IconButton } from "@mui/material";
+import { alertOpenState } from "../../store/AlertOpen";
+import { useRecoilState } from "recoil";
+import * as S from "./AlertBar.styles";
+
+function AlertBar() {
+  const [alertOpen, setAlertOpen] = useRecoilState(alertOpenState);
+  return (
+    <div>
+      <S.StyledSnackbar
+        open={alertOpen}
+        autoHideDuration={100000}
+        onClose={() => setAlertOpen(false)}
+      >
+        <S.StyledAlert
+          severity="info"
+          icon={false}
+          action={
+            <S.StyledButtons>
+              <S.StyledButton
+                variant="contained"
+                onClick={() => {
+                  setAlertOpen(false);
+                }}
+                color="success"
+                size="small"
+              >
+                승인
+              </S.StyledButton>
+              <S.StyledButton
+                variant="contained"
+                onClick={() => {
+                  setAlertOpen(false);
+                }}
+                color="error"
+                size="small"
+              >
+                거절
+              </S.StyledButton>
+            </S.StyledButtons>
+          }
+        >
+          정동겸님이 방 공유를 요청했어요!
+        </S.StyledAlert>
+      </S.StyledSnackbar>
+    </div>
+  );
+}
+
+export default AlertBar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,19 @@ import type { AppProps } from "next/app";
 import GlobalStyle from "../styles/globals";
 import { theme } from "../styles/theme";
 import { ThemeProvider } from "styled-components";
+import { RecoilRoot } from "recoil";
+import AlertBar from "../components/alert/AlertBar";
 
 export default function App({ Component, pageProps }: AppProps) {
 
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <AlertBar />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </RecoilRoot>
   );
 }
